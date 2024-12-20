3808eddb8b16ceced7e51244dda5e28076d13d8a
Dilki687 <chamodidilki44@gmail.com>
2024-12-20 21:27:52 +0530

    Initial commit

---
+++ src/main/java/com/food/model/ContactInformation.java
@@ -0,0 +1,16 @@
+package com.food.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+@Embeddable
+@Data
+public class ContactInformation {
+    private String email;
+
+    private String mobile;
+
+    private String twitter;
+
+    private String instagram;
+}
