e93ffd6874ab87b1f2099c8b1f88183b432dbd64
Dilki687 <chamodidilki44@gmail.com>
2025-01-07 22:58:24 +0530

    change in ContactInformation

--- src/main/java/com/food/model/ContactInformation.java
+++ src/main/java/com/food/model/ContactInformation.java
@@ -13,4 +13,37 @@ public class ContactInformation {
     private String twitter;
 
     private String instagram;
+
+    // Manually added getters and setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+    public String getInstagram() {
+        return instagram;
+    }
+
+    public void setInstagram(String instagram) {
+        this.instagram = instagram;
+    }
 }
