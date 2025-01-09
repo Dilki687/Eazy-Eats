a4de10382ea5c7026ca08233aef007eff3c755b9
Dilki687 <chamodidilki44@gmail.com>
2025-01-09 22:28:03 +0530

    food controller

---
+++ src/main/java/com/food/request/CreateFoodRequest.java
@@ -0,0 +1,23 @@
+package com.food.request;
+
+import com.food.model.Category;
+import com.food.model.IngredientsItem;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CreateFoodRequest {
+
+    private String name;
+    private String description;
+    private Long price;
+
+    private Category category;
+    private List<String> images;
+
+    private Long restaurantId;
+    private boolean vegetarin;
+    private boolean seasional;
+    private List<IngredientsItem> ingredients;
+}
