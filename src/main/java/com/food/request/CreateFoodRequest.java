0fa49c54cdcd6d25a0b00e7645a53137fa0d8afe
Dilki687 <chamodidilki44@gmail.com>
2025-01-09 22:33:52 +0530

    food controller

--- src/main/java/com/food/request/CreateFoodRequest.java
+++ src/main/java/com/food/request/CreateFoodRequest.java
@@ -14,6 +14,79 @@ public class CreateFoodRequest {
     private Long price;
 
     private Category category;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    public Long getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(Long restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public boolean isVegetarin() {
+        return vegetarin;
+    }
+
+    public void setVegetarin(boolean vegetarin) {
+        this.vegetarin = vegetarin;
+    }
+
+    public boolean isSeasional() {
+        return seasional;
+    }
+
+    public void setSeasional(boolean seasional) {
+        this.seasional = seasional;
+    }
+
+    public List<IngredientsItem> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<IngredientsItem> ingredients) {
+        this.ingredients = ingredients;
+    }
+
     private List<String> images;
 
     private Long restaurantId;
